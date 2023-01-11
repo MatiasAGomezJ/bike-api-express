8a734148a20175ac10f7561edf724aa7f7e85272
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2023-01-11 22:45:31 +0100

    chore: :art: Moves code inside src folder

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var bikesRouter = require('./routes/BikeRouter');
+var bikesRouter = require('./src/routes/BikeRouter');
 
 var app = express();
 
