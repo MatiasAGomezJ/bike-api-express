8e6ebdd2474f1e8741327c3d1ad65bb896940284
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2023-01-16 18:10:41 +0100

    refactor(BikeController.js): :fire: Remove try catch from every controller

--- src/controllers/BikeController.js
+++ src/controllers/BikeController.js
@@ -3,57 +3,28 @@ const BikeService = require("../service/BikeService");
 module.exports = {
     listBikes: async (req, res) => {
         const filter = req.query;
-
-        try {
-            const bikes = await BikeService.listBikes(filter);
-
-            bikes ? res.status(200).send(bikes) : res.status(400).send({ msg: "Error al obtener las bicicletas" });
-        } catch (error) {
-            res.status(500).send(error);
-        }
+        const bikes = await BikeService.listBikes(filter);
+        bikes ? res.status(200).send(bikes) : res.status(400).send({ msg: "Error al obtener las bicicletas" });
     },
     getBikeById: async (req, res) => {
         const bikeId = req.params.id;
-        try {
-            const bike = await BikeService.getBikeById(bikeId);
-
-            bike ? res.status(200).send(bike) : res.status(400).send({ msg: "Error al obtener la bicicleta" });
-        } catch (error) {
-            res.status(500).send(error);
-        }
+        const bike = await BikeService.getBikeById(bikeId);
+        bike ? res.status(200).send(bike) : res.status(400).send({ msg: "Error al obtener la bicicleta" });
     },
     createBike: async (req, res) => {
         const body = req.body;
-
-        try {
-            const savedBike = await BikeService.createBike(body);
-
-            savedBike ? res.status(201).send({ bike: savedBike }) : res.status(400).send({ msg: "No se ha guardado la bicicleta" });
-        } catch (error) {
-            res.status(500).send(error);
-        }
+        const savedBike = await BikeService.createBike(body);
+        savedBike ? res.status(201).send({ bike: savedBike }) : res.status(400).send({ msg: "No se ha guardado la bicicleta" });
     },
     updateBike: async (req, res) => {
         const bikeId = req.params.id;
         const body = req.body;
-
-        try {
-            const updatedBike = await BikeService.updateBike(bikeId, body);
-
-            updatedBike ? res.status(200).send({ bike: updatedBike }) : res.status(400).send({ msg: "No se ha actualizado la bicicleta" });
-        } catch (error) {
-            res.status(500).send(error);
-        }
+        const updatedBike = await BikeService.updateBike(bikeId, body);
+        updatedBike ? res.status(200).send({ bike: updatedBike }) : res.status(400).send({ msg: "No se ha actualizado la bicicleta" });
     },
     deleteBike: async (req, res) => {
         const bikeId = req.params.id;
-
-        try {
-            const deletedBike = await BikeService.deleteBike(bikeId);
-
-            deletedBike ? res.status(200).send({ bike: deletedBike }) : res.status(400).send({ msg: "No se ha borrado la bicicleta" });
-        } catch (error) {
-            res.status(500).send(error);
-        }
-    }
+        const deletedBike = await BikeService.deleteBike(bikeId);
+        deletedBike ? res.status(200).send({ bike: deletedBike }) : res.status(400).send({ msg: "No se ha borrado la bicicleta" });
+    },
 };
