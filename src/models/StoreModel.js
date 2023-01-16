50aae17c3a5c7a4a32194e8b388bc2268e616488
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2023-01-16 21:23:05 +0100

    fix(StoreModel.js): :bug: Misspelled names

--- src/models/StoreModel.js
+++ src/models/StoreModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const DispoSchema = new Schema({
+const StoreSchema = new Schema({
     name: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Dispo", DispoSchema);
+module.exports = mongoose.model("Store", StoreSchema);
