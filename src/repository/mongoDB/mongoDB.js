ae0364e8df989aef521a03a17e1e0c9178b58475
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2023-01-16 23:52:14 +0100

    feat(mongoDB.js): :sparkles: Create interface

--- src/repository/mongoDB/mongoDB.js
+++ src/repository/mongoDB/mongoDB.js
@@ -1,6 +1,14 @@
 var mongoose = require("mongoose");
 
-var mongoDB = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@sandbox.glkvp.mongodb.net/bike-app-${process.env.NODE_ENV}?retryWrites=true&w=majority`;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+mongoose.set('strictQuery', false);
+
+var mongoDBUri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@sandbox.glkvp.mongodb.net/bike-app-${process.env.NODE_ENV}?retryWrites=true&w=majority`;
+
+module.exports = {
+    connect: () => {
+        mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    },
+    disconnect: () => {
+        mongoose.disconnect();
+    }
+}
