4a3f23a3f3fcfbdd5e25399d4be96cbf37d2ccb7
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2023-01-16 17:09:54 +0100

    refactor(BikeService.js): :recycle: Update functions to match last commit

--- src/service/BikeService.js
+++ src/service/BikeService.js
@@ -1,19 +1,20 @@
 const DB = require("../repository/repository");
+const Bike = require("../models/BikeModel");
 
 module.exports = {
     listBikes: async (filter) => {
-        return await DB.listBikes(filter);
+        return await DB.list(Bike, filter);
     },
     getBikeById: async (bikeId) => {
-        return await DB.getBikeById(bikeId);
+        return await DB.getById(Bike, bikeId);
     },
     createBike: async (body) => {
-        return await DB.createBike(body);
+        return await DB.create(Bike, body);
     },
     updateBike: async (bikeId, body) => {
-        return await DB.updateBike(bikeId, body);
+        return await DB.update(Bike, bikeId, body);
     },
     deleteBike: async (bikeId) => {
-        return await DB.deleteBike(bikeId);
+        return await DB.delete(Bike, bikeId);
     },
 };
