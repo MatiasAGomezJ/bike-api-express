1218142239ab4110a11e301b1d2ae98e0c74f5be
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2023-01-16 21:57:37 +0100

    feat(StockService.js): :sparkles: Create stock service

---
+++ src/service/StockService.js
@@ -0,0 +1,20 @@
+const DB = require("../repository/repository");
+const Stock = require("../models/StockModel");
+
+module.exports = {
+    listStock: async (filter) => {
+        return await DB.list(Stock, filter);
+    },
+    getStockItemById: async (stockId) => {
+        return await DB.getById(Stock, stockId);
+    },
+    createStockItem: async (body) => {
+        return await DB.create(Stock, body);
+    },
+    updateStockItem: async (stockId, body) => {
+        return await DB.update(Stock, stockId, body);
+    },
+    deleteStockItem: async (stockId) => {
+        return await DB.delete(Stock, stockId);
+    },
+};
