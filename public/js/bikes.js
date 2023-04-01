ea9ddf39d4fe503739d5e2047ddf7bf2ba25b388
MatiasAGomeJ <mgomez@cifpfbmoll.eu>
2023-04-01 19:41:10 +0200

    feat: :lipstick: Style bikes page

--- public/js/bikes.js
+++ public/js/bikes.js
@@ -8,25 +8,57 @@ async function getData() {
     }
 }
 async function show() {
-    container = document.getElementById("bike-container");
+    container = document.getElementsByClassName("bike-container")[0];
     let data = await getData();
     data.forEach((bike) => {
-        let div = document.createElement("div");
-        div.classList.add("bike");
-        
-        for (const item in bike) {
-            const element = bike[item];
-            if (item === "_id") {
-                let h2 = document.createElement("h2");
-                h2.innerHTML = element;
-                div.appendChild(h2);
+        let divImage = document.createElement("div");
+        divImage.classList.add("bike-image");
+        let img = document.createElement("img");
+        img.src = "nope";
+        divImage.appendChild(img);
+
+        let divInfo = document.createElement("div");
+        divInfo.classList.add("bike-info");
+        let divProperties = document.createElement("div");
+        divProperties.classList.add("bike-properties");
+
+        for (const key in bike) {
+            const value = bike[key];
+            if (key === "_id") {
+                let title = document.createElement("p");
+                title.innerHTML = value;
+                title.classList.add("bike-title");
+                divInfo.appendChild(title);
                 continue;
             }
             let p = document.createElement("p");
-            p.innerHTML = `${item}: ${element}`;
-            div.appendChild(p);
+            p.classList.add("bike-property");
+            p.innerHTML = `<span class="bike-key">${key}:</span>${value}`;
+            divProperties.appendChild(p);
         }
-        container.appendChild(div);
+        divInfo.appendChild(divProperties);
+
+        let divButtons = document.createElement("div");
+        divButtons.classList.add("bike-buttons");
+        let modifyButton = document.createElement("a");
+        modifyButton.classList.add("bike-button");
+        modifyButton.innerHTML = "Modificar";
+        modifyButton.href = `http://localhost/bikes/modify/${bike._id}`;
+        let deleteButton = document.createElement("a");
+        deleteButton.classList.add("bike-button");
+        deleteButton.innerHTML = "Eliminar";
+        deleteButton.href = `http://localhost/bikes/delete/${bike._id}`;
+        divButtons.appendChild(modifyButton);
+        divButtons.appendChild(deleteButton);
+
+        let bikeItem = document.createElement("div");
+        bikeItem.classList.add("bike-item");
+
+        bikeItem.appendChild(divImage);
+        bikeItem.appendChild(divInfo);
+        bikeItem.appendChild(divButtons);
+
+        container.appendChild(bikeItem);
     });
 }
 
