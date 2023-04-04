6f405339e20524eb6e7589e76221a51b51bc6e1d
MatiasAGomeJ <mgomez@cifpfbmoll.eu>
2023-04-04 15:51:13 +0200

    refactor: :recycle: More refactors

--- public/js/bikes.js
+++ public/js/bikes.js
@@ -18,42 +18,38 @@ const BIKE_FORM_FIELDS = {
     wheels: "",
 };
 
-async function getBikes() {
-    return await utils.getAll(utils.ENTITIES.BIKE);
-}
+const init = async () => {
+    const entitiesContainer = document.querySelector(".entities-container");
+    if (entitiesContainer) {
+        const bikes = await getBikes();
+        utils.show(entitiesContainer, bikes);
+    }
 
-async function getBike(_id) {
-    return await utils.getOne(utils.ENTITIES.BIKE, _id);
-}
+    const form = document.querySelector(".entity-form-container");
+    if (form) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get("_id");
+        const data = id ? await getBike(id) : BIKE_FORM_FIELDS;
+        utils.showForm(form, data);
+    }
 
-const form = document.getElementsByClassName("entity-form-container")[0];
+    const deleteElement = document.querySelector(".entity-delete");
+    if (deleteElement) {
+        const id = new URLSearchParams(window.location.search).get("_id");
+        await deleteBike(id);
+    }
+};
 
-const entitiesContainer =
-    document.getElementsByClassName("entities-container")[0];
+const getBikes = async () => {
+    return utils.getAll(utils.ENTITIES.BIKE);
+};
 
-const deleteElement = document.getElementsByClassName("entity-delete")[0];
-async function deleteBike() {
-    let data = await getBikes();
-    let id = new URLSearchParams(window.location.search).get("_id");
-    fetch(`http://localhost/api/bike/${id}`, {
-        method: "DELETE",
-    }).then((response) => {
-        if (!response.ok)
-            throw new Error(
-                `DELETE request failed with status ${response.status}`
-            );
-        window.location.href = "/bikes";
-    });
-}
+const getBike = async (id) => {
+    return utils.getOne(utils.ENTITIES.BIKE, id);
+};
 
-if (entitiesContainer) utils.show(entitiesContainer, await getBikes());
+const deleteBike = async (id) => {
+    await utils.deleteOne(utils.ENTITIES.BIKE, id);
+};
 
-if (form) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const _id = urlParams.get("_id");
-    let isUpdate = _id;
-    const data = isUpdate ? await getBike(_id) : BIKE_FORM_FIELDS;
-    utils.showForm(form, data);
-}
-
-if (deleteElement) deleteBike();
+init();
--- public/js/utils.js
+++ public/js/utils.js
@@ -4,162 +4,216 @@ export const ENTITIES = {
     STOCK: "stock",
 };
 
-export async function getAll(entity) {
+async function apiRequest(method, entity, data, _id = null) {
+    const url = `http://localhost/api/${entity}${_id ? `/${_id}` : ""}`;
+    const options = {
+        method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    };
     try {
-        let response = await fetch(`http://localhost/api/${entity}`);
-        let data = await response.json();
-        return data;
+        let response;
+        if (method === "GET") {
+            response = await fetch(url);
+        } else {
+            response = await fetch(url, options);
+        }
+        if (!response.ok) {
+            throw new Error(
+                `${method} request failed with status ${response.status}`
+            );
+        }
+        return response.json();
     } catch (error) {
         console.error(error);
+        return error;
     }
 }
 
-export async function getOne(entity, _id) {
-    try {
-        let response = await fetch(`http://localhost/api/${entity}/${_id}`);
-        let data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
+async function createBike() {
+    const formData = new FormData(
+        document.getElementsByClassName("entity-form")[0]
+    );
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+        data[key] = value;
     }
+    await apiRequest("POST", "bike", data);
+    window.location.href = "/bikes";
 }
 
-export async function show(parent, data) {
-    data.forEach((item) => {
-        let divImage = document.createElement("div");
-        divImage.classList.add("entity-image");
-        let img = document.createElement("img");
-        img.src = "/resources/bikes/1.png";
-        divImage.appendChild(img);
+async function updateBike() {
+    const formData = new FormData(
+        document.getElementsByClassName("entity-form")[0]
+    );
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+        data[key] = value;
+    }
+    const _id = data["_id"];
+    await apiRequest("PUT", "bike", data, _id);
+    window.location.href = "/bikes";
+}
 
-        let info = document.createElement("div");
-        info.classList.add("entity-info");
-        let divProperties = document.createElement("div");
-        divProperties.classList.add("entity-properties");
+function createButtons(item) {
+    const buttons = document.createElement("div");
+    buttons.classList.add("entity-buttons");
+    const modifyButton = createButton(
+        "Modificar",
+        `http://localhost/bikes/update?_id=${item._id}`
+    );
+    const deleteButton = createButton(
+        "Eliminar",
+        `http://localhost/bikes/delete?_id=${item._id}`
+    );
+    buttons.appendChild(modifyButton);
+    buttons.appendChild(deleteButton);
+    return buttons;
+}
 
-        for (const key in item) {
-            const value = item[key];
-            if (key === "_id") {
-                let title = document.createElement("p");
-                title.innerHTML = value;
-                title.classList.add("entity-title");
-                info.appendChild(title);
-                continue;
-            }
-            let p = document.createElement("p");
+function createButton(label, url) {
+    const button = document.createElement("a");
+    button.classList.add("entity-button");
+    button.innerHTML = label;
+    button.href = url;
+    return button;
+}
+
+function createEntityItem(item) {
+    const entityItem = document.createElement("div");
+    entityItem.classList.add("entity-item");
+
+    const divImage = createDivImage();
+    const info = createInfo(item);
+    const buttons = createButtons(item);
+
+    entityItem.appendChild(divImage);
+    entityItem.appendChild(info);
+    entityItem.appendChild(buttons);
+
+    return entityItem;
+}
+
+function createDivImage() {
+    const divImage = document.createElement("div");
+    divImage.classList.add("entity-image");
+    const img = document.createElement("img");
+    img.src = "/resources/bikes/1.png";
+    divImage.appendChild(img);
+    return divImage;
+}
+
+function createInfo(item) {
+    const info = document.createElement("div");
+    info.classList.add("entity-info");
+    const divProperties = document.createElement("div");
+    divProperties.classList.add("entity-properties");
+
+    for (const key in item) {
+        const value = item[key];
+        if (key === "_id") {
+            const title = document.createElement("p");
+            title.innerHTML = value;
+            title.classList.add("entity-title");
+            info.appendChild(title);
+        } else {
+            const p = document.createElement("p");
             p.classList.add("entity-property");
             p.innerHTML = `<span class="entity-key">${key}:</span>${value}`;
             divProperties.appendChild(p);
         }
-        info.appendChild(divProperties);
+    }
 
-        let buttons = document.createElement("div");
-        buttons.classList.add("entity-buttons");
-        let modifyButton = document.createElement("a");
-        modifyButton.classList.add("entity-button");
-        modifyButton.innerHTML = "Modificar";
-        modifyButton.href = `http://localhost/bikes/update?_id=${item._id}`;
-        let deleteButton = document.createElement("a");
-        deleteButton.classList.add("entity-button");
-        deleteButton.innerHTML = "Eliminar";
-        deleteButton.href = `http://localhost/bikes/delete?_id=${item._id}`;
-        buttons.appendChild(modifyButton);
-        buttons.appendChild(deleteButton);
+    info.appendChild(divProperties);
+    return info;
+}
 
-        let entityItem = document.createElement("div");
-        entityItem.classList.add("entity-item");
+function createElement(tagName, className = "") {
+    const element = document.createElement(tagName);
+    element.className = className;
+    return element;
+}
 
-        entityItem.appendChild(divImage);
-        entityItem.appendChild(info);
-        entityItem.appendChild(buttons);
+function createFormElement(tagName, className) {
+    const form = createElement(tagName, className);
+    return form;
+}
 
+function createInputHiddenElement(key, value) {
+    const input = createElement("input");
+    input.type = "hidden";
+    input.name = key;
+    input.value = value;
+    return input;
+}
+
+function createLabelElement(key) {
+    const label = createElement("label");
+    label.htmlFor = key;
+    label.textContent = `${key}: `;
+    return label;
+}
+
+function createInputElement(key, value) {
+    const input = createElement("input");
+    input.type = typeof value === "number" ? "number" : "text";
+    input.name = key;
+    input.id = key;
+    input.value = value;
+    return input;
+}
+
+function createSubmitButton(data) {
+    const button = createElement("button");
+    button.type = "submit";
+    button.textContent = data["_id"] ? "Actualizar" : "Crear";
+    return button;
+}
+
+// Export functions
+
+export async function getAll(entity) {
+    return await apiRequest("GET", entity);
+}
+
+export async function getOne(entity, _id) {
+    return await apiRequest("GET", entity, {}, _id);
+}
+
+export async function deleteOne(entity, _id) {
+    await apiRequest("DELETE", entity, {}, _id);
+    window.location.href = "/bikes";
+}
+
+export async function show(parent, data) {
+    for (const item of data) {
+        const entityItem = createEntityItem(item);
         parent.appendChild(entityItem);
-    });
-}
-
-function createBike() {
-    const formData = new FormData(
-        document.getElementsByClassName("entity-form")[0]
-    );
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-        data[key] = value;
     }
-    fetch("http://localhost/api/bike", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then((response) => {
-        if (!response.ok)
-            throw new Error(
-                `POST request failed with status ${response.status}`
-            );
-        window.location.href = "/bikes";
-    });
-}
-
-function updateBike() {
-    const formData = new FormData(
-        document.getElementsByClassName("entity-form")[0]
-    );
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-        data[key] = value;
-    }
-    fetch(`http://localhost/api/bike/${data["_id"]}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then((response) => {
-        if (!response.ok)
-            throw new Error(
-                `PUT request failed with status ${response.status}`
-            );
-        window.location.href = "/bikes";
-    });
 }
 
 export async function showForm(form, data) {
-    let updateForm = document.createElement("form");
-    updateForm.classList.add("entity-form");
+    const updateForm = createFormElement("form", "entity-form");
 
-    for (const key in data) {
-        const e = data[key];
-        if (key == "_id" || key == "__v") {
-            let input = document.createElement("input");
-            input.setAttribute("type", "hidden");
-            input.setAttribute("name", key);
-            input.setAttribute("value", e);
+    Object.entries(data).forEach(([key, value]) => {
+        if (key === "_id" || key === "__v") {
+            const input = createInputHiddenElement(key, value);
             updateForm.appendChild(input);
         } else {
-            let label = document.createElement("label");
-            label.setAttribute("for", key);
-            label.innerHTML = `${key}: `;
-            let input = document.createElement("input");
-            input.setAttribute(
-                "type",
-                typeof e == "number" ? "number" : "text"
-            );
-            input.setAttribute("name", key);
-            input.setAttribute("id", key);
-            input.setAttribute("value", e);
+            const label = createLabelElement(key);
+            const input = createInputElement(key, value);
             label.appendChild(input);
             updateForm.appendChild(label);
         }
-    }
-    let br = document.createElement("br");
-    updateForm.appendChild(br);
-    let button = document.createElement("button");
-    button.setAttribute("type", "submit");
-    button.innerHTML = isUpdate ? "Actualizar" : "Crear";
+    });
+
+    updateForm.appendChild(createElement("br"));
+    const button = createSubmitButton(data);
     updateForm.appendChild(button);
+
     form.appendChild(updateForm);
     updateForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        isUpdate ? updateBike() : createBike();
+        data["_id"] ? updateBike() : createBike();
     });
 }
